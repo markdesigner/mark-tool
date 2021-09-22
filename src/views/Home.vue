185ab246633f2e6409995615a68ff183fedc45be
mark.chang <weisheng1202@gmail.com>
2021-09-23 00:24:52 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="copy__container">
+      <div class="copy__item" v-for="(item, index) in copyList" :key="index">
+        <input
+          class="copy__text"
+          type="text"
+          v-model="item.value"
+          placeholder="copy words"
+        />
+        <button
+          v-clipboard:copy="item.value"
+          v-clipboard:success="copySuccess"
+          v-clipboard:error="copyError"
+        >
+          copy
+        </button>
+        <button @click="handleDelete(index)">delete</button>
+      </div>
+    </div>
+
+    <button @click="handleAdd">add</button>
+    <button @click="handleSave">save</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      copyList: [{ value: "" }],
+    };
   },
+  methods: {
+    handleAdd() {
+      this.copyList.push({ value: "" });
+    },
+    copySuccess() {
+    },
+    copyError() {
+      alert("copy Error");
+    },
+    handleSave() {
+      const stringifyData = JSON.stringify(this.copyList);
+      localStorage.setItem("copyList", stringifyData);
+    },
+    handleRestore() {
+      const rawData = localStorage.getItem("copyList");
+      if (rawData) {
+        try {
+          const copyList = JSON.parse(rawData);
+          this.copyList = copyList;
+        } catch {
+          localStorage.removeItem("copyList");
+        }
+      }
+    },
+    handleDelete(index) {
+      this.copyList.splice(index, 1);
+    },
+  },
+  mounted() {
+    this.handleRestore();
+  },
+  components: {},
 };
 </script>
+<style lang="scss" scoped>
+.copy {
+  &__text {
+    width: 50%;
+  }
+}
+</style>
