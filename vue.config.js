ceb8ab83e29bc33b93d9d10581ded37d460b65d8
Mark <mark.chang@vivotek.com>
2021-09-23 12:02:44 +0800

    add github pages

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// place it with same level of package.json 
+// vue.config.js 設定指南 : https://cli.vuejs.org/zh/config/#%E5%85%A8%E5%B1%80-cli-%E9%85%8D%E7%BD%AE
+module.exports = {
+    // publicPath 為 project-name，根目錄地址為上傳的網域
+    publicPath: '/mark-tool/',
+};
